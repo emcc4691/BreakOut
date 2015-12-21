41d526c40852528ae19d18618d1573a3dd26a982
Emma McConnell <emma.mcconnell@geeks.ltd.uk>
2015-12-21 13:47:26 +0000

    Add base HTML page, styling and initial JS. Add less file to compiler config.

---
+++ Game.js
@@ -0,0 +1,24 @@
+﻿/* Initialise */
+
+function Game() {
+    this.canvas = document.getElementById('canvas');
+}
+
+Game.prototype.findContext = function () {
+    return this.canvas.getContext('2d');
+}
+
+Game.prototype.buildBlock = function (a, b, c, d) {
+    var ctx = this.findContext();
+    ctx.beginPath();
+    ctx.rect(a, b, c, d);
+    ctx.fillStyle = "#FFF";
+    ctx.fill();
+    ctx.closePath();
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var game = new Game();
+
+    game.buildBlock(10, 20, 30, 40);
+});
