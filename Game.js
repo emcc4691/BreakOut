2b9e893916a455f7c0c79f0c2063e4790f87467f
Emma McConnell <emma.mcconnell@geeks.ltd.uk>
2015-12-23 18:02:09 +0000

    Add method to request animation frame.

--- Game.js
+++ Game.js
@@ -44,6 +44,16 @@ Game.prototype.setUpBlocks = function(xMargin, yMargin, width, height) {
     }
 }
 
+window.requestAnimFrame = (function () {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            return window.setTimeout(callback, 1000 / 60);
+        };
+})();
 
 // Let's play the game
 
