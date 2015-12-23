a39091058c26e7003999872f08137f16a80f98cd
Emma McConnell <emma.mcconnell@geeks.ltd.uk>
2015-12-23 18:01:50 +0000

    Move static properties to constructor. Rename method.

--- Game.js
+++ Game.js
@@ -2,13 +2,19 @@
 
 function Game() {
     this.canvas = document.getElementById('canvas');
+    this.rowNumber = 5;
+    this.dx = 2;
+    this.dy = -2;
+    this.xStartBall = 150;
+    this.yStartBall = 140;
+    this.radiusBall = 3;
 }
 
 Game.prototype.findContext = function () {
     return this.canvas.getContext('2d');
 }
 
-Game.prototype.buildBlock = function (x, y, width, height) {
+Game.prototype.drawBlock = function (x, y, width, height) {
     var ctx = this.findContext();
     ctx.beginPath();
     ctx.rect(x, y, width, height);
@@ -17,19 +23,33 @@ Game.prototype.buildBlock = function (x, y, width, height) {
     ctx.closePath();
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    var game = new Game();
-    var xMargin = 3;
-    var yMargin = 2;
-    var width = 38;
-    var height = 10;
+Game.prototype.drawBall = function () {
+    var ctx = this.findContext();
+    ctx.beginPath();
+    ctx.arc(this.xStartBall, this.yStartBall, this.radiusBall, 0, Math.PI * 2);
+    ctx.fillStyle = "#099";
+    ctx.fill();
+    ctx.closePath();
 
-    for (var j = 0; j <= 4; j++)
-    {
-        rowHeight = (j + 1) * yMargin + j * height;
-        for (var i = 0; i <= 6; i++)
-        {
-            game.buildBlock((i + 1) * xMargin + i * width + (j % 2 == 0 ? 0 : 10), rowHeight, width, height);
+    this.xStartBall += this.dx;
+    this.yStartBall += this.dy;
+}
+
+Game.prototype.setUpBlocks = function(xMargin, yMargin, width, height) {
+    for (var j = 0; j <= this.rowNumber - 1; j++) {
+        var rowHeight = (j + 1) * yMargin + j * height;
+        for (var i = 0; i <= 6; i++) {
+            this.drawBlock((i + 1) * xMargin + i * width + (j % 2 == 0 ? 0 : 10), rowHeight, width, height);
         }
     }
+}
+
+
+// Let's play the game
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var game = new Game();
+
+    game.setUpBlocks(3, 2, 38, 10);
+    game.drawBall();
 });
