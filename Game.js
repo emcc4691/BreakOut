cffc30bfc89d89305443609689435619edfc037e
Emma McConnell <emma.mcconnell@geeks.ltd.uk>
2016-01-06 17:57:57 +0000

    Refactor. Add move logic. Add collision detection logic.

--- Game.js
+++ Game.js
@@ -21,9 +21,12 @@ function Block(xStart, yStart, width, height) {
 function Ball() {
     this.xStartPosition = 150;
     this.yStartPosition = 140;
+    this.xCurrentPosition = this.xStartPosition;
+    this.yCurrentPosition = this.yStartPosition;
+    this.speed = 0.5;
     this.radius = 3;
-    this.dx = 2;
-    this.dy = -2;
+    this.dx = this.speed * 3;
+    this.dy = -this.speed;
     this.fillStyle = "#099";
 }
 
@@ -35,12 +38,102 @@ Block.prototype.drawBlock = function (context) {
     context.closePath();
 }
 
-Ball.prototype.drawBall = function (context, xStart, yStart) {
+Block.prototype.checkCollisionWithBlock = function (ball) {
+
+    var ballNorth = ball.yCurrentPosition - ball.radius;
+    var ballSouth = ball.yCurrentPosition + ball.radius;
+    var ballWest = ball.xCurrentPosition - ball.radius;
+    var ballEast = ball.xCurrentPosition + ball.radius;
+       
+    var blockLower = this.yStart + this.height;
+    var blockUpper = this.yStart;
+    var blockLeft = this.xStart;
+    var blockRight = this.xStart + this.width;
+
+    // check sides
+    if (ballNorth >= blockUpper && ballSouth <= blockLower)
+    {
+        // check left boundary
+        if (ballEast >= blockLeft && ballEast <= blockRight) {
+            ball.changeDirection(true, false);
+            game.blocks.splice(game.blocks.indexOf(this), 1);
+        }
+
+        // check right boundary
+        if (ballWest >= blockLeft && ballWest <= blockRight) {
+            ball.changeDirection(true, false);
+            game.blocks.splice(game.blocks.indexOf(this), 1);
+        }
+    }
+
+    // check top and bottom
+    if (ballWest >= blockLeft && ballEast <= blockRight)
+    {
+        // check top
+        if (ballSouth >= blockUpper && ballSouth <= blockLower)
+        {
+            ball.changeDirection(false, true);
+            game.blocks.splice(game.blocks.indexOf(this), 1);
+        }
+
+        // check bottom
+        if (ballNorth >= blockUpper && ballNorth <= blockLower) {
+            ball.changeDirection(false, true);
+            game.blocks.splice(game.blocks.indexOf(this), 1);
+        }
+    }
+
+}
+
+Ball.prototype.drawBall = function (context) {
     context.beginPath();
-    context.arc(xStart, yStart, this.radius, 0, Math.PI * 2);
+    context.arc(this.xCurrentPosition, this.yCurrentPosition, this.radius, 0, Math.PI * 2);
     context.fillStyle = this.fillStyle;
     context.fill();
     context.closePath();
+}
+
+Ball.prototype.moveBall = function () {
+    this.xCurrentPosition += this.dx;
+    this.yCurrentPosition += this.dy;
+}
+
+Ball.prototype.checkCollisionWithWall = function() {
+    
+    var width = game.context.canvas.width;
+    var height = game.context.canvas.height;
+
+    // check left boundary
+    if (this.xCurrentPosition - this.radius <= 0)
+        this.changeDirection(true, false);
+
+    // check right boundary
+    if (this.xCurrentPosition + this.radius >= width)
+        this.changeDirection(true, false);
+
+    // check top boundary
+    if (this.yCurrentPosition - this.radius <= 0)
+        this.changeDirection(false, true);
+
+    // check bottom boundary
+    if (this.yCurrentPosition + this.radius >= height)
+        this.changeDirection(false, true);
+}
+
+Ball.prototype.checkCollisionWithBlocks = function () {
+    for(var i = 0; i < game.blocks.length; i++)
+    {
+        var block = game.blocks[i];
+        block.checkCollisionWithBlock(this);
+    }
+}
+
+Ball.prototype.changeDirection = function (changeX, changeY) {
+    if (changeX == true)
+        this.dx = -this.dx;
+    
+    if (changeY == true)
+        this.dy = -this.dy;
 }
 
 Game.prototype.createBlocks = function(xMargin, yMargin, width, height) {
@@ -62,16 +155,38 @@ Game.prototype.drawBlocks = function () {
 }
 
 Game.prototype.initialise = function () {
+    game.ball.xCurrentPosition = game.ball.xStartPosition;
+    game.ball.yCurrentPosition = game.ball.yStartPosition;
     game.createBlocks(3, 2, 38, 10);
-    game.drawBlocks();
-    game.ball.drawBall(game.context, game.ball.xStartPosition, game.ball.yStartPosition);
+    game.draw();
+}
+
+Game.prototype.draw = function () {
+    game.drawBlocks()
+    game.ball.drawBall(game.context);
 }
 
 Game.prototype.clear = function () {
     game.context.clearRect(0, 0, game.canvas.width, game.canvas.height);
 }
 
-window.requestAnimFrame = (function () {
+Game.prototype.move = function () {
+    this.clear();
+    this.ball.moveBall();
+    // check for collision with wall
+    this.ball.checkCollisionWithWall();
+    // check for collision with block
+    this.ball.checkCollisionWithBlocks();
+    this.draw();
+}
+
+Game.prototype.start = function () {
+    game.move();
+    
+    window.requestAnimFrame(game.start)
+}
+
+window.requestAnimFrame = (function (callback) {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.mozRequestAnimationFrame ||
@@ -82,7 +197,15 @@ window.requestAnimFrame = (function () {
         };
 })();
 
-// Let's play the game
+Initialise = function () {
+    game.initialise();
+    game.start();
+}
+
+Clear = function () {
+    // TODO end animation
+    game.clear();
+}
 
 document.addEventListener("DOMContentLoaded", function (event) {
     var startButton = document.getElementById('start');
@@ -93,14 +216,4 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     startButton.addEventListener("click", Initialise);
     stopButton.addEventListener("click", Clear);
-
-    //window.requestAnimFrame(game.drawBall());
 });
-
-Initialise = function () {
-    game.initialise();
-}
-
-Clear = function () {
-    game.clear();
-}
